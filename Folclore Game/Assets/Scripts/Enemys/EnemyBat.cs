17af157d0ce5929bf6978bfb63e4ac32722c1ad5
Pedro Policello <88664809+PedroPolicello@users.noreply.github.com>
2024-05-13 21:00:00 -0300

    Parallax & Ajuste Inimigos

--- Folclore Game/Assets/Scripts/Enemys/EnemyBat.cs
+++ Folclore Game/Assets/Scripts/Enemys/EnemyBat.cs
@@ -1,15 +1,18 @@
-using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class EnemyBat : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private int maxLife;
+    [SerializeField] private float fireRate;
+    [SerializeField] private GameObject poopPrefab;
+    private float nextFire = 0.0F;
 
     private int currentLife;
     private Transform transform;
     private GameObject target;
     private SpriteRenderer spriteRenderer;
+
     void Awake()
     {
         transform = GetComponent<Transform>();
@@ -17,11 +20,13 @@ public class EnemyBat : MonoBehaviour
         target = GameObject.FindGameObjectWithTag("Player");
         currentLife = maxLife;
     }
+
     void Update()
     {
         FollowPlayer();
         FlipX();
     }
+
     void FlipX()
     {
         if (target.transform.position.x > transform.position.x)
@@ -33,10 +38,31 @@ public class EnemyBat : MonoBehaviour
             spriteRenderer.flipX = false;
         }
     }
+
     void FollowPlayer()
     {
         transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        if (transform.position.y <= 1)
+        {
+            transform.position = new Vector2(transform.position.x, 1);
+        }
+
+        if (transform.position.x >= target.transform.position.x - 1 &&
+            transform.position.x <= target.transform.position.x + 1)
+        {
+            Shoot();
+        }
     }
+
+    void Shoot()
+    {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(poopPrefab, transform.position, transform.rotation);
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Spell"))
@@ -44,12 +70,14 @@ public class EnemyBat : MonoBehaviour
             TakeDamame(1);
         }
     }
+
     void TakeDamame(int dmg)
     {
         currentLife -= dmg;
         CheckLife();
         print(currentLife);
     }
+
     void CheckLife()
     {
         if (currentLife <= 0)
--- Folclore Game/Assets/Scripts/Player/PlayerInputsControl.cs
+++ Folclore Game/Assets/Scripts/Player/PlayerInputsControl.cs
@@ -14,6 +14,7 @@ public class PlayerInputsControl : MonoBehaviour
     
     private void Awake()
     {
+        DontDestroyOnLoad(gameObject);
         instance = this;
         SetInput();
     }
